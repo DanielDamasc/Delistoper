8ce7fd8d54474f4c2368b1e1aa173650bb338831
DanielDamasc <dandamasceno04@gmail.com>
2026-02-12 01:15:34 -0300

    build: Installing and adding JWT on module for using auth tokens

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     imports: [
-        UserModule
+        UserModule,
+
+        // Configuração do JWT (bearer token)
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '1d' }, // Token expira em 1 dia
+        }),
     ],
     controllers: [
         AuthController
