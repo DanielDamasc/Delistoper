09a4fe1e67728ced3704ac36f8e2cadf386cb4ce
DanielDamasc <dandamasceno04@gmail.com>
2026-02-13 02:33:48 -0300

    feat: Guard for deal with protected requests

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Request } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectService } from './project.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('project')
 export class ProjectController {
 
     constructor(private readonly projectService: ProjectService) {}
 
+    @UseGuards(AuthGuard)
     @Post('create')
     @HttpCode(HttpStatus.CREATED)
     async createProject(@Body() createProjectDto: CreateProjectDto, @Request() req) {
-        return this.projectService.create(createProjectDto, req.user.id);
+        return this.projectService.create(createProjectDto, req.user.sub);
     }
 }
